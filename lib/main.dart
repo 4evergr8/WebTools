86bd54a5c292421128b9752c140ad8ba4716ddbe
Joshhshh <137018415+4evergr8@users.noreply.github.com>
2025-03-28 14:24:42 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import '/screens/dns_screen.dart';
-import '/screens/address_screen.dart';
-import '/screens/port_screen.dart';
-import '/screens/python_screen.dart';
-import '/screens/history_screen.dart';
+import 'widgets/bottom_nav_bar.dart';
 import 'util.dart'; // 假设 util.dart 包含 createTextTheme 函数
 import 'theme.dart'; // 假设 theme.dart 包含 MaterialTheme 类
 
@@ -26,66 +22,6 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: brightness == Brightness.light ? theme.light() : theme.dark(),
       home: BottomNavBar(), // 使用底部导航栏组件
-    );
-  }
-}
-
-// 底部导航栏组件
-class BottomNavBar extends StatefulWidget {
-  const BottomNavBar({super.key});
-
-  @override
-  _BottomNavBarState createState() => _BottomNavBarState();
-}
-
-class _BottomNavBarState extends State<BottomNavBar> {
-  int _selectedIndex = 0;
-
-  static final List<Widget> _widgetOptions = <Widget>[
-    DNSScreen(),
-    AddressScreen(),
-    PortScreen(),
-    PythonScreen(),
-    HistoryScreen(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _widgetOptions.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.network_check),
-            label: 'DNS',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.location_on),
-            label: '地址',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings_ethernet),
-            label: '端口',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.code),
-            label: 'Python插件',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            label: '历史',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
     );
   }
 }
