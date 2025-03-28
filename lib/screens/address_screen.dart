9b5a7c706ee76a36e532f026cd1a087636fca247
Joshhshh <137018415+4evergr8@users.noreply.github.com>
2025-03-28 14:14:06 +0800

    Update address_screen.dart

--- lib/screens/address_screen.dart
+++ lib/screens/address_screen.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 
 class AddressScreen extends StatelessWidget {
-  const AddressScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('地址'),
+        title: Text('地址', style: theme.textTheme.headlineMedium),
+        backgroundColor: theme.colorScheme.inversePrimary,
       ),
       body: Center(
-        child: Text('地址 Screen'),
+        child: Text(
+          '地址 Screen',
+          style: theme.textTheme.bodyLarge,
+        ),
       ),
     );
   }
