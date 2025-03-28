e4dbbe01be499b5062ad8c4f6bd048ec06fbae7e
Joshhshh <137018415+4evergr8@users.noreply.github.com>
2025-03-28 14:14:09 +0800

    Update port_screen.dart

--- lib/screens/port_screen.dart
+++ lib/screens/port_screen.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 
 class PortScreen extends StatelessWidget {
-  const PortScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('端口'),
+        title: Text('端口', style: theme.textTheme.headlineMedium),
+        backgroundColor: theme.colorScheme.inversePrimary,
       ),
       body: Center(
-        child: Text('端口 Screen'),
+        child: Text(
+          '端口 Screen',
+          style: theme.textTheme.bodyLarge,
+        ),
       ),
     );
   }
