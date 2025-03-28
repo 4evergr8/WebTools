889c53a3369bfbd77ee5982e357d4d89b387f774
Joshhshh <137018415+4evergr8@users.noreply.github.com>
2025-03-28 14:24:44 +0800

    Update python_screen.dart

--- lib/screens/python_screen.dart
+++ lib/screens/python_screen.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 
 class PythonScreen extends StatelessWidget {
-  const PythonScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Python插件'),
+        title: Text('Python插件', style: theme.textTheme.headlineMedium),
+        backgroundColor: theme.colorScheme.inversePrimary,
       ),
       body: Center(
-        child: Text('Python插件 Screen'),
+        child: Text(
+          'Python插件 Screen',
+          style: theme.textTheme.bodyLarge,
+        ),
       ),
     );
   }
